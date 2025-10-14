b2045a3b1cd066b9b2f93e6a787ad1e2487238b6
bolin yang <boliny@163.com>
2025-10-14 20:43:36 +0800

    create

--- test_back/del_all.sh
+++
@@ -1,8 +0,0 @@
-test_task=$(ls -l |grep ^d |awk '{print$9}')
-
-for task in ${test_task};do
-    cd ${task}
-    rm ${task} run -r
-    cd ../
-done
-
--- test_back/test_all.sh
+++
@@ -1,8 +0,0 @@
-test_task=$(ls -l |grep ^d |awk '{print$9}')
-
-for task in ${test_task};do
-    cd ${task}
-    python ../../main.py -d . -i run.in.EXAMPLE1 system.data system.in.init system.in.settings -o temp -t ${task} -p pre
-    cd ../
-done
-
